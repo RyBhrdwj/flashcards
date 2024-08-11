bf8a14c45e17eab135770d0ecf878f18dcaefefa
RyBhrdwj <25ricky08@gmail.com>
2024-08-12 02:12:16 +0530

    update package.json

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/database");
-const flashcardRouter = require("./routes/router");
+const cookieParser = require('cookie-parser');
+
 
 dotenv.config();
+
+const flashcardRouter = require("./routes/flashcardRoutes");
+const adminRouter = require("./routes/adminRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use("/flashcard", flashcardRouter);
+app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is Live!");
